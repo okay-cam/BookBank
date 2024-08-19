b6bd9cc5f9e024867ed38a2da39f99dd62e0eb86
Anneke Muir <prq9377@autuni.ac.nz>
2024-08-19 14:40:55 +1200

    Basic login GUI
    Added username field, password field, and submit button

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,22 @@ import React from "react";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <h1>Login</h1>
+      <label>Username</label>
+      <br />
+      <input type="text" />
+      <br />
+      <br />
+      <label>Password</label>
+      <br />
+      <input type="password" />
+      <br />
+      <br />
+      <input type="submit" />
+    </>
+  );
 };
 
 export default Login;
