84df130ee3b06fd68002774d9dc0c741a074e548
cam-arzo <camoarrow4586@gmail.com>
2024-08-20 23:20:54 +1200

    Add auth context
    This can be imported when handling auth cases

---
+++ src/contexts/auth_context/index.jsx
@@ -0,0 +1,51 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { auth } from "../../config/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+// stores currentUser (firebase auth data or null),
+// userLoggedIn (bool),
+// and loading (bool)
+const AuthContext = React.createContext();
+
+// Retrieve auth values. Import this when using auth!
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+// Provide data to the auth context
+export function AuthProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState(null);
+    const [userLoggedIn, setUserLoggedIn] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, initializeUser);
+        return unsubscribe;
+    }, [])
+
+    async function initializeUser(user) {
+        if (user) {
+            setCurrentUser({ ...user });
+            setUserLoggedIn(true);
+        }
+        else {
+            setCurrentUser(null);
+            setUserLoggedIn(false);
+        }
+        setLoading(false);
+    }
+
+    const auth_values = {
+        // contains firebase auth data (uid, email, etc), or null
+        currentUser,
+        userLoggedIn,
+        loading
+    }
+
+    return (
+        <AuthContext.Provider value={auth_values}>
+            {!loading && children}
+        </AuthContext.Provider>
+    )
+
+}
