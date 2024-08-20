1b16094e2dec85ec6b119ca1dc078f0b27f46b15
cam-arzo <camoarrow4586@gmail.com>
2024-08-21 00:17:53 +1200

    Add unfunctional login code
    Fix firebase.ts since my work was undone randomly

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,21 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// !! I don't think this is needed
+//import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.REACT_APP_API_KEY,
-  authDomain: import.meta.env.REACT_APP_AUTH_DOMAIN,
-  projectId: import.meta.env.REACT_APP_PROJECT_ID,
-  storageBucket: import.meta.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: import.meta.env.REACT_APP_APP_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-// Initialize Database
-export const db = getFirestore(app);
+export { app, auth }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,7 +5,7 @@ import "../styles/general.css";
 import { Navigate, Link } from "react-router-dom";
 
 // get auth functions for checking login state
-import { doSignInWithEmailAndPassword, doSignInWithGoogle } from '../config/firebase'
+import { doSignInWithEmailAndPassword } from '../config/auth'
 import { useAuth } from '../contexts/auth_context'
 
 const Login = () => {
@@ -22,42 +22,42 @@ const Login = () => {
   //  {errorMessage}
   // )}
   // so it checks if the error message exists, and if so, shows it.
-  // Cam can set the text through code later
-  const [errorMessage, setErrorMessage] = useState('')
+  // const [errorMessage, setErrorMessage] = useState('')
 
 
   // Attempt sign in after pressing submit button
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if(!isSigningIn) {
-        // Attempt sign-in here
-        setIsSigningIn(true);
-        try {
-            await doSignInWithEmailAndPassword(email, password);
-        } catch (error) {
-            setIsSigningIn(false);
-            // show correct error message depending on the issue
-            switch (error.code) {
-                case 'auth/invalid-credential':
-                    setErrorMessage('Invalid credentials. Please check your input.');
-                    break;
-                case 'auth/user-not-found':
-                    setErrorMessage('No user found with this email.');
-                    break;
-                case 'auth/wrong-password':
-                    setErrorMessage('Incorrect password. Please try again.');
-                    break;
-                case 'auth/too-many-requests':
-                    setErrorMessage('Too many failed login attempts. Please try again later.');
-                    break;
-                case 'auth/network-request-failed':
-                    setErrorMessage('Network error. Please check your connection and try again.');
-                    break;
-                default:
-                    setErrorMessage('An unexpected error occurred. Please try again.');
-                    break;
-            }
-        }
+      // Attempt sign-in here
+      setIsSigningIn(true);
+      try {
+          await doSignInWithEmailAndPassword(email, password);
+      } catch (error) {
+          setIsSigningIn(false);
+          console.log(error);
+          // show correct error message depending on the issue
+          // switch (error.code) {
+          //     case 'auth/invalid-credential':
+          //         setErrorMessage('Invalid credentials. Please check your input.');
+          //         break;
+          //     case 'auth/user-not-found':
+          //         setErrorMessage('No user found with this email.');
+          //         break;
+          //     case 'auth/wrong-password':
+          //         setErrorMessage('Incorrect password. Please try again.');
+          //         break;
+          //     case 'auth/too-many-requests':
+          //         setErrorMessage('Too many failed login attempts. Please try again later.');
+          //         break;
+          //     case 'auth/network-request-failed':
+          //         setErrorMessage('Network error. Please check your connection and try again.');
+          //         break;
+          //     default:
+          //         setErrorMessage('An unexpected error occurred. Please try again.');
+          //         break;
+          // }
+      }
     }
   }
 
@@ -66,28 +66,30 @@ const Login = () => {
     <>
       <h1>Kia ora!</h1>
       <h2>Welcome to BookBank</h2>
-      <label>Email</label>
-      <br />
-      <input
-        type="text"
-        value={email}
-        onChange={(e) => { setEmail(e.target.value) }}
-      />
-      <br />
-      <br />
-      <label>Password</label>
-      <br />
-      <input type="password"
-        value={password}
-        onChange={(e) => { setPassword(e.target.value) }} />
-      <br />
-      <br />
-      <Link to="/home">
+      <form onSubmit={onSubmit}>
+        <label>Email</label>
+        <br />
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <br />
+        <label>Password</label>
+        <br />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <br />
         <input
           type="submit"
-          onSubmit={onSubmit}
+          value="Sign In"
         />
-      </Link>
+      </form>
     </>
   );
 };
