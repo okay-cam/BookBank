239bee46799a10c05ddf0bfad3211838de82bcbb
cam-arzo <camoarrow4586@gmail.com>
2024-08-20 23:37:01 +1200

    Update email and password states when user enters text

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,28 +6,37 @@ import { Navigate, Link } from "react-router-dom";
 
 // get auth functions for checking login state
 // TODO: add firebase auth functions for signing in
-import { useAuth } from '../../../contexts/authContext'
+import { useAuth } from '../contexts/auth_context'
 
 const Login = () => {
 
-  const { userLoggedIn } = useAuth()
+  // Stores web page states which are used during account authentication
+  // const { userLoggedIn } = useAuth()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [isSigningIn, setIsSigningIn] = useState(false)
-  const [errorMessage, setErrorMessage] = useState('')
+  // isSigningIn may be used to prevent extra login attempts when processing authentication
+  // const [isSigningIn, setIsSigningIn] = useState(false)
+  // TODO: Add a label for error messages to be displayed. Cam can set the text through code later
+  // const [errorMessage, setErrorMessage] = useState('')
 
   return (
     <>
       <h1>Kia ora!</h1>
       <h2>Welcome to BookBank</h2>
-      <label>Username</label>
+      <label>Email</label>
       <br />
-      <input type="text" />
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => { setEmail(e.target.value) }}
+      />
       <br />
       <br />
       <label>Password</label>
       <br />
-      <input type="password" />
+      <input type="password"
+        value={password}
+        onChange={(e) => { setPassword(e.target.value) }} />
       <br />
       <br />
       <Link to="/home">
