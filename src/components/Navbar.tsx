f35811dd4ee0d55b4258b081cbc65824640c1a37
cam-arzo <camoarrow4586@gmail.com>
2024-08-21 13:03:56 +1200

    Add logout button to test auth functionality
    Sign in & sign out functions as intended. Logout button may be moved into its own component for clarity if it makes the navbar code too messy.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,30 @@ import React from "react";
 import "../styles/general.css";
 import styles from "../styles/navbar.module.css";
 
+// used for signing out - may be moved later
+import { useNavigate } from 'react-router-dom'
+import { doSignOut } from '../config/auth'
+import { useAuth } from '../contexts/auth_context'
+
 const Navbar = () => {
-  return <div className={styles.navbar}>Navbar</div>;
+
+  const navigate = useNavigate()
+  const { userLoggedIn } = useAuth();
+
+  return (
+  <>
+    <div className={styles.navbar}> Navbar
+      {/* Added temp code for testing signout functionality -Cam */}
+      <br></br>
+      {
+        userLoggedIn &&
+        <>
+          <button onClick={() => { doSignOut().then(() => { navigate('/') }) }}>Logout</button>
+        </>
+      }
+    </div>
+  </>
+  ) 
 };
 
 export default Navbar;
