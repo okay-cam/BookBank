9ebf5d4755a9386c223e2899a5de08d193661a69
Anneke Muir <prq9377@autuni.ac.nz>
2024-08-22 08:32:36 +1200

    Create listing form
    Create listing page: Created form for title, author, code and description inputs (no image yet),
    Submit button prints inputs to console.
    Login page: Added htmlFor attributes to labels,
    Added name and id attributes to email and password inputs
    Changed type of email input to email instead of text

--- src/pages/CreateListing.tsx
+++ src/pages/CreateListing.tsx
@@ -1,17 +1,105 @@
 import React from "react";
+import { useState } from "react";
 import "../styles/general.css";
 import styles from "../styles/listing.module.css";
 
-const CreateListing = () => {
+interface ListingData {
+  title: string;
+  authors: string;
+  courseCode: string;
+  description: string;
+}
+
+const CreateListing: React.FC = () => {
+  const [listingData, setListingData] = useState<ListingData>({
+    title: "",
+    authors: "",
+    courseCode: "",
+    description: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setListingData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(
+      listingData
+    );
+  };
+
   return (
     <>
       <main className={styles.gridContainer}>
         <div className={styles.aside}>
           <p>Image would be inserted here</p>
-          <button>Upload image</button>
+          <input
+            type="file"
+            id="image"
+            name="image"
+            accept="image/png, image/jpg"
+          />
         </div>
         <div className={styles.content}>
           <h1>Create listing</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="title">Textbook title:</label>
+            <br />
+            <input
+              type="text"
+              id="title"
+              name="title"
+              value={listingData.title}
+              onChange={handleChange}
+            />
+            <br />
+            <br />
+
+            <label htmlFor="authors">Author/s:</label>
+            <br />
+            <input
+              type="text"
+              id="authors"
+              name="authors"
+              value={listingData.authors}
+              onChange={handleChange}
+            />
+            <br />
+            <br />
+
+            <label htmlFor="courseCode">Course code:</label>
+            <br />
+            <input
+              type="text"
+              id="courseCode"
+              name="courseCode"
+              value={listingData.courseCode}
+              onChange={handleChange}
+            />
+            <br />
+            <br />
+
+            <label htmlFor="description">Description:</label>
+            <br />
+            <input
+              type="text"
+              id="description"
+              name="description"
+              value={listingData.description}
+              onChange={handleChange}
+            />
+            <br />
+            <br />
+
+            <input type="submit" value="Submit" />
+          </form>
         </div>
       </main>
     </>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 // importing bootstrap must be done before importing CSS files
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "../styles/general.css";
 import { Navigate, Link } from "react-router-dom";
 
 // get auth functions for checking login state
-import { doSignInWithEmailAndPassword } from '../config/auth'
-import { useAuth } from '../contexts/auth_context'
+import { doSignInWithEmailAndPassword } from "../config/auth";
+import { useAuth } from "../contexts/auth_context";
 
 const Login = () => {
-
   // Stores web page states which are used during account authentication
   // const { userLoggedIn } = useAuth()
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   // Used to prevent extra login attempts while processing auth. toggled when pressing submit
-  const [isSigningIn, setIsSigningIn] = useState(false)
+  const [isSigningIn, setIsSigningIn] = useState(false);
   // TODO: Add a label for error messages to be displayed.
   // it may look something like:
   // {errorMessage && (
@@ -24,74 +23,72 @@ const Login = () => {
   // so it checks if the error message exists, and if so, shows it.
   // const [errorMessage, setErrorMessage] = useState('')
 
-
   // Attempt sign in after pressing submit button
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if(!isSigningIn) {
+    if (!isSigningIn) {
       // Attempt sign-in here
       setIsSigningIn(true);
       try {
-          await doSignInWithEmailAndPassword(email, password);
+        await doSignInWithEmailAndPassword(email, password);
       } catch (error) {
-          setIsSigningIn(false);
-          console.log(error);
-          // show correct error message depending on the issue
-          // switch (error.code) {
-          //     case 'auth/invalid-credential':
-          //         setErrorMessage('Invalid credentials. Please check your input.');
-          //         break;
-          //     case 'auth/user-not-found':
-          //         setErrorMessage('No user found with this email.');
-          //         break;
-          //     case 'auth/wrong-password':
-          //         setErrorMessage('Incorrect password. Please try again.');
-          //         break;
-          //     case 'auth/too-many-requests':
-          //         setErrorMessage('Too many failed login attempts. Please try again later.');
-          //         break;
-          //     case 'auth/network-request-failed':
-          //         setErrorMessage('Network error. Please check your connection and try again.');
-          //         break;
-          //     default:
-          //         setErrorMessage('An unexpected error occurred. Please try again.');
-          //         break;
-          // }
+        setIsSigningIn(false);
+        console.log(error);
+        // show correct error message depending on the issue
+        // switch (error.code) {
+        //     case 'auth/invalid-credential':
+        //         setErrorMessage('Invalid credentials. Please check your input.');
+        //         break;
+        //     case 'auth/user-not-found':
+        //         setErrorMessage('No user found with this email.');
+        //         break;
+        //     case 'auth/wrong-password':
+        //         setErrorMessage('Incorrect password. Please try again.');
+        //         break;
+        //     case 'auth/too-many-requests':
+        //         setErrorMessage('Too many failed login attempts. Please try again later.');
+        //         break;
+        //     case 'auth/network-request-failed':
+        //         setErrorMessage('Network error. Please check your connection and try again.');
+        //         break;
+        //     default:
+        //         setErrorMessage('An unexpected error occurred. Please try again.');
+        //         break;
+        // }
       }
     }
-  }
-
+  };
 
   return (
     <>
-
       {/* {userLoggedIn && (<Navigate to={'/home'} replace={true} />)} */}
 
       <h1>Kia ora!</h1>
       <h2>Welcome to BookBank</h2>
       <form onSubmit={onSubmit}>
-        <label>Email</label>
+        <label htmlFor="email">Email</label>
         <br />
         <input
-          type="text"
+          type="email"
           value={email}
+          name="email"
+          id="email"
           onChange={(e) => setEmail(e.target.value)}
         />
         <br />
         <br />
-        <label>Password</label>
+        <label htmlFor="password">Password</label>
         <br />
         <input
           type="password"
           value={password}
+          name="password"
+          id="password"
           onChange={(e) => setPassword(e.target.value)}
         />
         <br />
         <br />
-        <input
-          type="submit"
-          value="Sign In"
-        />
+        <input type="submit" value="Sign In" />
       </form>
     </>
   );
