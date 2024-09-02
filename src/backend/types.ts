83dc95e56d3f12356b02001e94bab1efc1dfd647
cam-arzo <camoarrow4586@gmail.com>
2024-09-02 13:02:10 +1200

    Retrieve profile data and put it on the profile

--- src/backend/types.ts
+++ src/backend/types.ts
@@ -1,12 +1,16 @@
-// stores donor and listing info
-export interface Profile {
-    name: string;
-    degree: string;
-    email: string
-    // Add other profile details as needed
-  }
+// Structure of profile data (!! subject to change)
+// auth data (UID, email, sign in & account creation dates) are stores in the auth, not the profile
+export interface ProfileDetails {
+  name: string;
+  location: string;
+  university: string;
+  degree: string;
+  rating: number;
+  totalRatingsReceived: number;
+  totalDonations: number;
+}
   
-  export interface Listing {
+export interface Listing {
     id: number;
     title: string;
     authors: string;
@@ -15,3 +19,5 @@ export interface Profile {
     imageUrl: string;
     userID: string;
   }
+
+
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/profile.module.css";
 
+// backend imports for retrieving profile data & using ProfileData interface
+import { getProfileData } from "../backend/readData";
+import { ProfileData } from "../backend/types";
+
+import { auth } from "../config/firebase";
+
 const Profile = () => {
+
+  const [profileData, setProfileData] = useState<ProfileData | null>(null);
+
+  useEffect(() => {
+    const fetchAndSetProfileData = async () => {
+      const data = await getProfileData(auth.currentUser!.uid); // Pass the user ID to the function
+      setProfileData(data);
+    };
+
+    fetchAndSetProfileData();
+  }, []); // Empty dependency array means this effect runs once on mount
+
+
   return (
     <>
       <main className={styles.gridContainer}>
         <div className={styles.aside}></div>
         <div className={styles.content}>
           <h1>Profile</h1>
+
+          <br />
+
+          {/* profile data is retrieved here. this can be moved later */}
+          {profileData ? (
+          <div>
+            <p>Name: {profileData.name}</p>
+            <p>Location: {profileData.location}</p>
+            <p>University: {profileData.university}</p>
+            <p>Degree: {profileData.degree}</p>
+            <p>Rating: {profileData.rating}</p>
+            <p>Total Ratings Received: {profileData.totalRatingsReceived}</p>
+            <p>Total Donations: {profileData.totalDonations}</p>
+          </div>
+          ) : (
+            <p>Loading...</p>
+          )}
+
         </div>
       </main>
     </>
