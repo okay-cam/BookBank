c50f62f2d0ebe061f376c7e1d47370a17c1818a2
RoryTravis <rorandminer@gmail.com>
2024-09-03 16:59:00 +1200

    Adapt home page
    Home page no longer has long useEffect()

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,10 @@
-import React, { useEffect, useState } from "react";
 import Banner from "../components/Banner";
 import styles from "../styles/home.module.css";
 import CardContainer from "../components/CardContainer";
-import { Listing } from "../backend/types";
-import { getListings } from "../backend/listingService";
+import { useListings } from "../backend/readData";
 
 const Home = () => {
-  const [listings, setListings] = useState<Listing[]>([]); // Initialize state with an empty array
-
-  useEffect(() => {
-    const fetchListings = async () => {
-      const updatedListings = await getListings(); // Fetch and update listings
-      setListings(updatedListings); // Set the updated data to state
-      // console.log(updatedListings); // Log the updated listings with unique ids
-    };
-
-    fetchListings(); // Call the async function inside useEffect
-  }, []); // Empty dependency array means this effect runs once when the component mounts
+  const { listings, loading, error } = useListings();
 
   return (
     <main className={styles.gridContainer}>
