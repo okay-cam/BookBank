54b1e93f88b1318a33287465275a4d34a1fb198d
Anneke Muir <prq9377@autuni.ac.nz>
2024-09-05 19:31:17 +1200

    Show active listings on profile page
    Show active listings on profile page
    Removed underlines on components with links

--- src/components/DonorInfo.tsx
+++ src/components/DonorInfo.tsx
@@ -3,22 +3,25 @@ import styles from "../styles/listing.module.css";
 import { ProfileData } from "../backend/types";
 import { getProfileData } from "../backend/readData";
 import defaultImage from "../assets/default-image-path.jpg";
+import { Link } from "react-router-dom";
 
-interface DonorID {
+interface DonorIDProps {
   donorId: string;
 }
 
-const DonorInfo = () => {
+const DonorInfo = ({ donorId }: DonorIDProps) => {
   // need to get profile data by using getProfileData(donorId)
 
   return (
-    <div className={styles.donorInfo}>
-      <img src={defaultImage} className={styles.profilePic} />
-      <div className={styles.donorContent}>
-        <h1>Donor name</h1>
-        <p>Studying degree at university</p>
+    <Link to="/profile" className="no-underline">
+      <div className={styles.donorInfo}>
+        <img src={defaultImage} className={styles.profilePic} />
+        <div className={styles.donorContent}>
+          <h1>Donor name</h1>
+          <p>Studying degree at university</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
