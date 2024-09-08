72bf9248bc841d6fba3dad048507db7aebeff8b2
RoryTravis <rorandminer@gmail.com>
2024-09-09 00:32:45 +1200

    Convert modal id to listingId and delete
    Currently only functional when user closes the delete button once and then re-opens it again

--- src/backend/deleteData.ts
+++ src/backend/deleteData.ts
@@ -2,7 +2,11 @@ import { doc, deleteDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
 
 // Function to delete a document by its ID
-export const deleteListing = async (listingId: string) => {
+export const deleteListing = async (modalId: string) => {
+    // CURRENT MODAL ID FORMAT: "modal-DOCUMENT_ID-remove"
+    const seperatedId = modalId.split('-'); // Split the string by hyphens
+    const listingId = seperatedId[1]; // Extract the second part (middle text)
+
     console.log("Document starting deletion", listingId);
     try {
       const docRef = doc(db, "listings", listingId);
