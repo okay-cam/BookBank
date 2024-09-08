a946684d8e01a38b90204c961d7cabda70754c18
RoryTravis <rorandminer@gmail.com>
2024-09-09 00:25:32 +1200

    Create deleteListing function
    TODO // Convert Modal ID to normal id

--- src/backend/deleteData.ts
+++ src/backend/deleteData.ts
@@ -1,5 +1,17 @@
 import { doc, deleteDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
 
-
+// Function to delete a document by its ID
+export const deleteListing = async (listingId: string) => {
+    console.log("Document starting deletion", listingId);
+    try {
+      const docRef = doc(db, "listings", listingId);
+  
+      await deleteDoc(docRef);
+  
+      console.log("Document successfully deleted!");
+    } catch (error) {
+      console.error("Error deleting document: ", error);
+    }
+  };
 
