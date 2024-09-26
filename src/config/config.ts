f816760d7235b1a53de5d8fb8212085b5ade6847
RoryTravis <rorandminer@gmail.com>
2024-09-26 15:37:20 +1200

    Create config.ts
    Stores and exports constants

---
+++ src/config/config.ts
@@ -0,0 +1,42 @@
+
+// variables used in multiple listings
+const USER_ID = "userId";
+const COURSE_CODE = "courseCode";
+
+// Firestore Constants
+
+// points to where collection is located on database
+export class collection_name {
+    static listings = "listings";
+    static pins = "pins";
+    static users = "users";
+    static wishlist = "wishlist";
+}
+
+
+// field names for listings documents
+export class listings_field {
+    static id = "listingId";
+    static authors = "authors";
+    static courseCode = COURSE_CODE;
+    static description = "description";
+    static imageUrl = "imageUrl";
+    static title = "title";
+    static userId = USER_ID;
+}
+
+// field names for users documents
+export class users_field {
+    static degree = "degree";
+    static email = "email";
+    static joinDate = "joinDate";
+    static lastLoggedIn = "lastLoggedIn";
+    static location = "location";
+    static name = "name";
+    static overallRating = "overallRating";
+    static profilePic = "profilePic";
+    static totalDonations = "totalDonations";
+    static totalRatingsRecieved = "totalRatingsRecieved";
+    static university = "university";
+    static userId = USER_ID;
+}
