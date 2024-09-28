396db23afa4a9bed3869fcd1c110dd7a1ad5f2c4
RoryTravis <rorandminer@gmail.com>
2024-09-28 15:01:49 +1200

    create deleteImage function
    Calls getImageUrl to delete image
    
    TODO // create consistency between image storage and then update function to match

--- src/backend/deleteData.ts
+++ src/backend/deleteData.ts
@@ -1,22 +1,43 @@
 import { doc, deleteDoc } from "firebase/firestore";
-import { db } from "../config/firebase";
+import { db, storage } from "../config/firebase";
 import { collection_name } from "../config/config";
+import { ref, deleteObject } from "firebase/storage";
+import { getImageUrl } from "../backend/readData";
+
+export const deleteImage = async (imageUrl: string) => {
+  const imageRef = ref(storage, imageUrl);
+
+  deleteObject(imageRef)
+  .then(() => {
+    console.log('Image deleted successfully');
+  })
+  .catch((error) => {
+    console.error('Error deleting image:', error);
+  });
+
+};
 
 // Function to delete a document by its ID
 export const deleteListing = async (modalId: string) => {
-    // CURRENT MODAL ID FORMAT: "modal-DOCUMENT_ID-remove"
-    const seperatedId = modalId.split('-'); // Split the string by hyphens
-    const listingId = seperatedId[1]; // Extract the second part (middle text)
+  // CURRENT MODAL ID FORMAT: "modal-DOCUMENT_ID-remove"
+  const separatedId = modalId.split("-"); // Split the string by hyphens
+  const listingId = separatedId[1]; // Extract the second part (middle text)
 
-    console.log("Document starting deletion", listingId);
-    try {
-      const docRef = doc(db, collection_name.listings, listingId);
-  
-      await deleteDoc(docRef);
-  
-      console.log("Document successfully deleted!");
-    } catch (error) {
-      console.error("Error deleting document: ", error);
+  console.log("Document starting deletion", listingId);
+
+  try {
+    const docRef = doc(db, collection_name.listings, listingId);
+
+    const imageUrl = await getImageUrl(collection_name.listings, listingId);
+    if (imageUrl) {
+      await deleteImage(imageUrl);
     }
-  };
+
+      await deleteDoc(docRef);
+      console.log("Document successfully deleted!");
+  } catch (error) {
+    console.error("Error deleting document: ", error);
+  }
+};
+
 
