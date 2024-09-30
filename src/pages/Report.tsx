5d0ca1ff9ae792d2a394f53b951f5d682f3f35c3
Cam <camoarrow4586@gmail.com>
2024-09-30 16:36:53 +1300

    Display extra info on report page

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -51,7 +51,6 @@ const Report: React.FC = () => {
   const [notFound, setNotFound] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false); // Add submit loading state
   const [loading, setLoading] = useState(true); // Add page loading state
-  const [isListingReport, setIsListingReport] = useState(false); // Returns true if it is a listing report, false if a user report
   //   const [errorMessage, setErrorMessage] = useState("");
 
 
@@ -216,87 +215,6 @@ useEffect(() => {
 }, [id, type]);
 
 
-  //   // Load reported listing
-  //   if (type === 'listing') {
-  //     setIsListingReport(true);
-  //     const fetchListing = async () => {
-  //       try {
-  //         const listing = await getListingById(id);
-  //         if (listing) {
-  //           setListing(listing);
-  //           console.log("user: ", user)
-  //           setReportedProfileData(user); // set user data based on the owner of the listing
-  //           setListingReport(true);
-  //         } else {
-  //           setNotFound(true); // If no listing is found, set not found state
-  //         }
-  //       } catch (error) {
-  //         console.error("Error fetching listing:", error);
-  //         setNotFound(true);
-  //       } finally {
-  //         setLoading(false);
-  //         console.log("listing: ", listing)
-  //       }
-  //     };
-  //     fetchListing();
-  //   }
-        
-
-
-
-  // // For profiles, ONLY get reported profile data
-  // useEffect(() => {
-  //   if (type === "user" && id) {
-  //     // check if user and id exists
-  //     const fetchAndSetProfileData = async () => {
-  //       try {
-  //         const data = await getProfileData(id);
-  //         if (data) {
-  //           setReportedProfileData(data); // set profile data if it exists
-  //           setListingReport(false);
-  //         } else {
-  //           setNotFound(true); // If no profile is found, set not found state
-  //         }
-  //       } catch (error) {
-  //         console.error("Error fetching profile:", error);
-  //         setNotFound(true);
-  //       } finally {
-  //         setLoading(false);
-  //       }
-  //     };
-  //     fetchAndSetProfileData();
-  //   }
-  // }, [type, id]); // update if any url parameters change
-
-  // // For listings, get BOTH listing data and reported profile data
-  // useEffect(() => {
-  //   if (type === "listing" && id) {
-  //     const fetchListing = async () => {
-  //       try {
-  //         const listing = await getListingById(id);
-  //         if (listing) {
-  //           setListing(listing);
-  //           const user = await getProfileData(listing.userID);
-  //           console.log("user: ", user)
-  //           setReportedProfileData(user); // set user data based on the owner of the listing
-  //           setListingReport(true);
-  //         } else {
-  //           setNotFound(true); // If no listing is found, set not found state
-  //         }
-  //       } catch (error) {
-  //         console.error("Error fetching listing:", error);
-  //         setNotFound(true);
-  //       } finally {
-  //         setLoading(false);
-  //         console.log("listing: ", listing)
-  //       }
-  //     };
-  //     fetchListing();
-  //   }
-  // }, [type, id]);
-
-
-
   // If type is invalid or data not found, navigate to 404
   if (!["user", "listing"].includes(type!) || notFound) {
     console.log("report type doesnt exist: ", type)
@@ -341,14 +259,24 @@ useEffect(() => {
           <p>Loading...</p>
         ) : (
           <>
-          {isListingReport && (
+          {(type === 'listing')  && (
             <div>
-              <h1>Listing Information</h1>
-              <p>Title: {reportedListing?.title}</p>
+              <h1>Reported Listing Info</h1>
+              <p>{reportedListing?.title}</p>
+              <img
+                src={reportedListing?.imageUrl}
+                alt="Listing image"
+                style={{
+                  maxWidth: "100%",
+                  maxHeight: "300px",
+                  marginTop: "-10px",
+                }}
+              />
             </div>
           )}
 
-          <h1>Reported User Information</h1>
+          <br />
+          <h1>Reported User Info</h1>
           <p>Name: {reportedProfileData?.username}</p>
           </>
         )}
