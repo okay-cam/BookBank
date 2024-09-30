eec5c25ddae102907f46f6a3b533cf8939599a8b
RoryTravis <rorandminer@gmail.com>
2024-09-30 20:38:00 +1300

    Create interface again
    export type ProfileData = Omit<typeof users_field, 'prototype'>; // replaces type.ts did not allow for numbers/arrays

--- src/config/config.ts
+++ src/config/config.ts
@@ -16,6 +16,7 @@ export class fb_location {
 
 
 // field names for listings documents
+// Variable objects must match INTERFACE, eg static authors = "authors" therefor authors: string
 export class listings_field {
     static id = "listingId";
     static authors = "authors";
@@ -24,9 +25,24 @@ export class listings_field {
     static imageUrl = IMAGE_URL;
     static title = "title";
     static userID = USER_ID;
+    // optional
     static enquired = "enquired";
+    static pinned = "pinned";
 }
-export type Listing = Omit<typeof listings_field, 'prototype'>; // replaces type.ts
+
+export interface Listing{
+  listingId: string; 
+  authors: string; 
+  courseCode: string; 
+  description: string; 
+  imageUrl: string; 
+  title: string; 
+  userID: string; 
+  // optional
+  enquired?: string[]; 
+  pinned?: string[];
+}
+// export type Listing = Omit<typeof ListingsField, 'prototype'>; // replaces type.ts
 
 // field names for users documents
 export class users_field {
@@ -43,4 +59,20 @@ export class users_field {
     static university = "university";
     static userID = USER_ID;
 }
-export type ProfileData = Omit<typeof users_field, 'prototype'>; // replaces type.ts
+
+export interface ProfileData{
+    degree: string;
+    email: string;
+    joinDate: string;
+    lastLoggedIn: string;
+    location: string;
+    username: string;
+    overallRating: number;
+    iamgeUrl?: string;
+    totalDonations: number;
+    totalRatingsRecieved: number;
+    university: string;
+    userID: string;
+    wishlist?: string[]
+}
+// export type ProfileData = Omit<typeof users_field, 'prototype'>; // replaces type.ts, currently not in use as does not allow you to define variables as anything other than string
