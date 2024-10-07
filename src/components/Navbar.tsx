a04b0bde1ace531a3c991d51ad381764d0b64427
Cam <camoarrow4586@gmail.com>
2024-10-08 11:50:26 +1300

    Add icons to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -71,9 +71,9 @@ const Navbar = () => {
           </form>
 
           <div className={styles.navLinks}>
-            <Link to="/home" className={styles.navButton}>Home</Link>
-            <Link to="/saved" className={styles.navButton}>Saved listings</Link>
-            <Link to="/create" className={styles.navButton}>Create a listing</Link>
+            <Link to="/home" className={styles.navButton}>🏠 Home</Link>
+            <Link to="/saved" className={styles.navButton}>⭐ Saved listings</Link>
+            <Link to="/create" className={styles.navButton}>📙 Create a listing</Link>
             <div className={styles.profileDropdown}>
               <img src={profilePictureSource || defaultImage} alt="Profile" className={styles.profilePic} onClick={() => setDropdownOpen(!dropdownOpen)}></img>
             </div>
